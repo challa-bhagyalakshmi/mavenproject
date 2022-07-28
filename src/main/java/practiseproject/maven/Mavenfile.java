26296bf5d34e2510a9b8c9fc3268a18a1b031fbf
abc <abc@gmail.com>
2022-07-28 16:43:57 +0530

    added files

---
+++ src/main/java/practiseproject/maven/Mavenfile.java
@@ -0,0 +1,12 @@
+package practiseproject.maven;
+
+public class Mavenfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		 System.out.println( "new maven project" );
+	}
+
+}
